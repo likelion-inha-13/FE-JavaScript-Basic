1bd94d1983393fefe974fcbb894eed2f91a8d9c0
june <wnsgmlb501@naver.com>
2025-03-31 19:56:40 +0900

    p3

--- prob3.js
+++ prob3.js
@@ -4,16 +4,36 @@ let likelion = [
   { name: "전수빈", part: "FE_기획", age: 25 },
   { name: "김예린", part: "FE_홍보", age: 23 },
 ];
-
+let len = likelion.length;
+let n = 0;
 // 1번 문제 코드작성
-
+while(n<len){
+for(i=1;i<len;i++){
+  let tmp = likelion[i-1];
+  if(tmp.age>likelion[i].age){
+    likelion[i-1] = likelion[i];
+    likelion[i]=tmp;
+  }
+ }
+ n++;
+}
+  console.log(likelion);
 // 2번 문제 코드작성
 let result2 = [];
+for(i=0; i<len;i++){
+  if(likelion[i].age>=24){
+    result2.push(likelion[i]);
+  }
+}
 
 console.log(result2);
 
 // 3번 문제 코드작성
 let targetName = "전수빈";
 let result3 = null;
-
+for(i=0;i<len;i++){
+  if(likelion[i].name == targetName){
+    result3 = likelion[i].part;
+  }
+}
 console.log(result3);
