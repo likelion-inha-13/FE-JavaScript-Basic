c45250d2d41226bad509bef06f6e359606e5f305
june <wnsgmlb501@naver.com>
2025-03-31 20:02:17 +0900

    p1

--- prob1.js
+++ prob1.js
@@ -2,6 +2,12 @@
 
 function solution(num, total) {
   let answer = [];
+  let mid = total/num;
+  let start = mid-(num-1)/2
+  for(let i =0; i<num;i++){
+    answer.push(start);
+    start++;
+  }
 
   return answer;
 }
