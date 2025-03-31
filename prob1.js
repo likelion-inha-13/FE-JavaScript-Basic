28c818d565225ec5e0fd34fd80a7fb928786ccc9
minjaehan <tommy3942@naver.com>
2025-03-31 21:40:18 +0900

    js 과제 제출

--- prob1.js
+++ prob1.js
@@ -2,6 +2,16 @@
 
 function solution(num, total) {
   let answer = [];
-
+  let aver = total / num;
+  let start = aver- num/2+0.5;
+  for(let i=0; i<num;i++)
+  {
+    answer[i]=start+i; 
+  }
   return answer;
 }
+console.log(solution(3,12));// aver =4 //num=3 //total =12   
+console.log(solution(4,14));// aver =4 //num=3 //total =12   
+console.log(solution(5,5));// aver =4 //num=3 //total =12   
+// aver 3.5  num 4   total  14
+                              
