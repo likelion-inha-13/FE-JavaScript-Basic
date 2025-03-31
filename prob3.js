ef15bc2a7717dfe11f36ec3467446df5af8bd97f
강신석 <mayo9437@gmail.com>
2025-03-31 21:07:15 +0900

    js_study

--- prob3.js
+++ prob3.js
@@ -6,14 +6,32 @@ let likelion = [
 ];
 
 // 1번 문제 코드작성
+for (let i = 0; i < likelion.length - 1; i++) {
+  for (let j = i + 1; j < likelion.length; j++) {
+    if (likelion[i].age > likelion[j].age) {
+      let temp = likelion[i];
+      likelion[i] = likelion[j];
+      likelion[j] = temp;
+    }
+  }
+}
+console.log("1번 결과:", likelion);
 
 // 2번 문제 코드작성
 let result2 = [];
-
-console.log(result2);
+for (let i = 0; i < likelion.length; i++) {
+  if (likelion[i].age >= 24) {
+    result2.push(likelion[i]);
+  }
+}
+console.log("2번 결과:", result2);
 
 // 3번 문제 코드작성
 let targetName = "전수빈";
 let result3 = null;
-
-console.log(result3);
+for (let i = 0; i < likelion.length; i++) {
+  if (likelion[i].name === targetName) {
+    result3 = likelion[i].part;
+  }
+}
+console.log("3번 결과:", result3);
