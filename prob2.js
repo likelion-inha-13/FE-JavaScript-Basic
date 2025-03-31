2b9735a4e6d132ac66e1ccac7ed3b4666fcf04f3
june <wnsgmlb501@naver.com>
2025-03-31 19:40:14 +0900

    p2

--- prob2.js
+++ prob2.js
@@ -1,6 +1,9 @@
 //2. map활용 문제
 const numbers = [1, 2, 3, 4, 5];
 
-let answer;
+let answer = numbers.map(function(element){
+  if(element%2==0){return "짝수";}
+  else{return "홀수";}
+});
 
 console.log(answer);
