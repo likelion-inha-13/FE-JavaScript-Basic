8bfbd80c7a339bf4132a4e5bacc73986f40e091a
hongsujin2eeZyo <hong266770@gmail.com>
2025-04-01 00:01:21 +0900

    홍수진과제제출

--- prob1.js
+++ prob1.js
@@ -1,7 +1,17 @@
 //1. 함수 로직 작성 문제
 
 function solution(num, total) {
-  let answer = [];
 
-  return answer;
+  
+    //배열의 중앙 인덱스의 값임
+    let mNum= total/num;
+    let firstNum = mNum-((num-1)/2) //짝수개
+
+    let answer = [];
+
+    for(i=0;i<num;i++){
+        answer[i] = firstNum;
+        firstNum++;
+    }
+    return answer;
 }
