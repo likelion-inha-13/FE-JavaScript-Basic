8b4ed8df7dbbc484132f6fbe504b5c453bdd47a2
ohyesong <yesong9874@naver.com>
2025-04-01 18:38:58 +0900

    멋사 js기초세션 과제

--- prob3.js
+++ prob3.js
@@ -6,14 +6,38 @@ let likelion = [
 ];
 
 // 1번 문제 코드작성
+for (let i=0; i<likelion.length-1; i++) { // 3번
+    for (let j=0; j<likelion.length-1-i; j++) { // 2번씩
+      if (likelion[j].age > likelion[j+1].age) {
+          // 앞사람의 나이가 더 많으면 자리 교환
+          let storage = likelion[j];
+          likelion[j] = likelion[j+1];
+          likelion[j+1] = storage;
+      }
+    }
+}
+
+console.table(likelion);
 
 // 2번 문제 코드작성
 let result2 = [];
 
-console.log(result2);
+for (let i in likelion) {
+  if (likelion[i].age >= 24) {
+    result2.push(likelion[i]);
+  }
+}
+
+console.table(result2);
 
 // 3번 문제 코드작성
 let targetName = "전수빈";
 let result3 = null;
+
+for (let i in likelion) {
+  if (likelion[i].name === targetName){
+    result3 = likelion[i].part;
+  }
+}
 
 console.log(result3);
