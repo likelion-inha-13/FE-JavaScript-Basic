4355560b6f24bf0d28f5f43534128e8323d6a61e
BBoyeon <2boyeon2@gmail.com>
2025-04-02 21:43:38 +0900

    3/31 과제

--- prob1.js
+++ prob1.js
@@ -1,7 +1,22 @@
 //1. 함수 로직 작성 문제
 
 function solution(num, total) {
-  let answer = [];
+  let center = Math.floor(total/num); //Math.floor : 주어진 숫자보다 작거나 같은 수 중 가장 큰 정수 (버림)
+  let first = 0;
+
+  if (num % 2 === 1){
+    first = center - Math.floor(num / 2);} // 홀수일 떄 버림해야해서 Math.floor 함수 사용함함
+  else {
+    first = center - (num / 2) - 1;}; // 짝수일 때
+
+  let answer = []; // answer 배열 생성
+  answer[0]=first; // 이 배열 안에는 first를 넣을거다
+  for (let i=0; i<num; i++){
+    answer[i]=first+i // i가 num보다 작아질 떄까지, 배열 안에 더해서 넣음음
+  }
 
   return answer;
 }
+
+let result = solution(3,12);
+console.log(result);
--- prob3.js
+++ prob3.js
@@ -7,13 +7,44 @@ let likelion = [
 
 // 1번 문제 코드작성
 
+// 나이를 기준으로 오름차순 정렬 (선택 정렬)
+for (let i = 0; i < likelion.length; i++) {
+  for (let j = i + 1; j < likelion.length; j++) {
+    if (likelion[j].age < likelion[i].age) {
+      let temp = likelion[i];
+      likelion[i] = likelion[j];
+      likelion[j] = temp;
+    }
+  }
+}
+
+
+
+console.log("나이 오름차순 정렬 결과:", likelion);
+
+
 // 2번 문제 코드작성
 let result2 = [];
+for (let i = 0; i < likelion.length; i++) { // 배열의 길이보다 작을 때, i를 늘려가며 배열을 봄봄
+  if (likelion[i].age >= 24) { // i번째 요소의 나이가 24보다 작다면
+    result2.push(likelion[i]); // result2 배열에 그 i번째 요소를 넣음
+  }
+}
 
-console.log(result2);
+
+console.log(result2); 
+
 
 // 3번 문제 코드작성
 let targetName = "전수빈";
-let result3 = null;
+let result3 = 0;
+
+for (let i = 0; i < likelion.length; i++) { // 배열의 길이보다 작을 떄, i를 늘려가며 배열 안에 있는 타겟 이름을 찾음
+  if (likelion[i].name === targetName) { //배열의 i번째 요소의 이름과 타겟 이름이 같으면
+    result3 = likelion[i].part; // result3에 그 i번째 요소의 part를 넣음
+    break; // 그리고 반복문 끝냄냄
+  }
+}
 
 console.log(result3);
+
