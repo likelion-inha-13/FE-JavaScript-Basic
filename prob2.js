c4ce3af82703a1e06e918569affc40b84b9c1ef5
leedasom050115 <kiss9892@inha.edu>
2025-04-03 10:38:21 +0900

    이다솜과제

--- prob2.js
+++ prob2.js
@@ -1,6 +1,6 @@
 //2. map활용 문제
 const numbers = [1, 2, 3, 4, 5];
 
-let answer;
+let answer = numbers.map(num => num % 2 === 0 ? "짝수" : "홀수");
 
 console.log(answer);
--- prob3.js
+++ prob3.js
@@ -6,9 +6,26 @@ let likelion = [
 ];
 
 // 1번 문제 코드작성
+for (let i = 0; i < likelion.length; i++) {
+  for (let j = i + 1; j < likelion.length; j++) {
+    if (likelion[i].age > likelion[j].age) {
+      let temp = likelion[i];
+      likelion[i] = likelion[j];
+      likelion[j] = temp;
+    }
+  }
+}
+console.log("오름차순", likelion);
 
 // 2번 문제 코드작성
 let result2 = [];
+
+for (let i = 0; i < likelion.length; i++) {
+  if (likelion[i].age >= 24) {
+    result2.push(likelion[i]);
+  }
+}
+
 
 console.log(result2);
 
@@ -16,4 +33,10 @@ console.log(result2);
 let targetName = "전수빈";
 let result3 = null;
 
+for (let i = 0; i < likelion.length; i++) {
+  if (likelion[i].name === targetName) {
+    result3 = likelion[i].part;
+    break;  
+  }
+}
 console.log(result3);
