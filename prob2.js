cde6cc4c05b34d8b39f58b5c9438684f5f452536
yewonee <hyewon8685@naver.com>
2025-04-03 13:43:42 +0900

    과제 제출

--- prob2.js
+++ prob2.js
@@ -1,6 +1,14 @@
 //2. map활용 문제
 const numbers = [1, 2, 3, 4, 5];
 
-let answer;
+let answer= numbers.map(function(number){
+    return number%2==0? "짝수":"홀수";
+    
+    
+
+});
+
+
+
 
 console.log(answer);
